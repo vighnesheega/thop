32fd396859f238d823cd4cb907bddff061bdce44
Vighnesheega <vighnesheega@gmail.com>
2021-07-22 13:18:38 +0530

    thop

---
+++ r/for-loop-using-strings.R
@@ -0,0 +1,7 @@
+#For Loop using strings
+countries = c('India', 'U K', 'Japan', 'U S A', 'Australia', 'China')
+
+for (str in countries) {
+  print(paste("Countries are:  ", str))
+}
+print("----This is Coming from Outside the For Loop---")
---
+++ r/fundamental-operators.R
@@ -0,0 +1,15 @@
+#addition
+x=2+3
+print(x)
+
+#subtraction
+x=10-5
+print(x)
+
+#multiplication
+x=6*8
+print(x)
+
+#division
+x=45/5
+print(x)
